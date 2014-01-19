0d88b2b681a3ee4d1a1d2cb7585d751a2b8eace0
Pavel Plotnikov <PPG-plotnikov@yandex.ru>
2014-01-19 18:58:02 +0400

    Подправил сервер для отладки шаблонов и основной страницы. Добавил игнорирование логов в git. Добавил скрипт запуска сервера в консоли для Linux. Зафиксил баг с импортом js.

--- server/iff-tornado.py
+++ server/iff-tornado.py
@@ -9,13 +9,15 @@
 #       it-for-free 2014
 #----------------------------------------------------
 import tornado.ioloop
+import tornado.template
 import tornado.web
 import iffconfig.settings
 
 
 class MainHeader(tornado.web.RequestHandler):
     def get(self):
-        pass
+        loader = tornado.template.Loader("../static/html/")
+        self.write(loader.load("mainpage.html").generate())
 
 
 if __name__ == "__main__":
@@ -23,4 +25,5 @@ if __name__ == "__main__":
         (r"/", MainHeader),
     ], **iffconfig.settings.app_settings)
     application.listen(8888)
+    print("I'm ready")
     tornado.ioloop.IOLoop.instance().start()
